67617e999b3e61cf3a5a3f5f34da3b02993e2e0a
Ariel Betti <ariel.betti@gmail.com>
2023-02-04 17:46:52 -0300

    feat(login-page): create functional login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,60 @@
-import { useSignWithGoogleMutation } from "@/queries/useSignWithGoogleMutation";
-import { supabase } from "@/lib/initSupabase";
-import { useAuthActions } from "@/store/user";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
-export default function Home() {
-  const signWithGoogle = useSignWithGoogleMutation();
+// routes
+import { ROUTES } from "@/routes";
+
+// zustand: states
+import { useUser } from "@/store/user";
+
+// queries & mutations
+import { useSignMutation } from "@/queries/useSignMutation";
+import { useGetUserSessionQuery } from "@/queries/useGetUserSessionQuery";
+
+// components
+import { Button, LoadingStatus } from "@/components";
+
+// ::
+const Home = () => {
+  const router = useRouter();
+  const user = useUser();
+
+  const userSession = useGetUserSessionQuery();
+  const signWithGoogle = useSignMutation();
+
+  useEffect(() => {
+    userSession.refetch();
+    if (user?.email) router.push(ROUTES.DASHBOARD);
+  }, [user]);
 
   return (
-    <>
-      <button onClick={() => signWithGoogle.mutate()}>Logar com google</button>
-      <button onClick={() => supabase.auth.getUser()}>Get user</button>
-      <button onClick={() => supabase.auth.signOut()}>Logout</button>
-    </>
+    <main className="pattern-wavy pattern-blue-500 dark:pattern-zinc-800 dark:pattern-bg-zinc-900 pattern-bg-zinc-900 pattern-opacity-100 pattern-size-16">
+      <div className="flex container mx-auto px-4 items-center justify-center min-h-screen">
+        <div className="max-w-2xl flex flex-col gap-2 items-center">
+          <div className="flex flex-col gap-2 mb-10 items-center justify-center text-center">
+            <h1 className="text-5xl">
+              Welcome to <span className="text-blue-600 font-bold">XAMA</span>
+            </h1>
+            <p className="text-xl">Use Xama to communicate with your folks!</p>
+          </div>
+          <div className="w-full flex gap-2 flex-wrap justify-center">
+            <Button
+              className="max-w-xs w-full font-semibold bg-blue-600 border-b-4 border-blue-900"
+              onClick={() => signWithGoogle.mutate("google")}
+            >
+              Login with Google
+            </Button>
+          </div>
+          {userSession.isFetching && (
+            <div className="flex gap-5 items-center justify-start">
+              <LoadingStatus />
+              <p className="animate-pulse text-lg">Loading...</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </main>
   );
-}
+};
+
+export default Home;
