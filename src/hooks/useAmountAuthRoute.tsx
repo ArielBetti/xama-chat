69b9fd0797d05fd01f529a08e7a4d58262df163d
Ariel Betti <ariel.betti@gmail.com>
2023-02-04 17:44:39 -0300

    feat: create hook to persist auth states in application

---
+++ src/hooks/useAmountAuthRoute.tsx
@@ -0,0 +1,19 @@
+import { IUser } from '@/interfaces';
+import { useUser } from '@/store/user';
+import { useEffect, useState } from 'react'
+
+const useAmountAuthRoute = () => {
+  const user = useUser();
+  const [userLoader, setUserLoader] = useState<IUser | null>();
+
+  useEffect(() => {
+    setUserLoader(user);
+  }, [user]);
+
+
+  return {
+    userLoader,
+  }
+}
+
+export default useAmountAuthRoute;
