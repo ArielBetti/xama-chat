166715a56a97d75e77fa9e65960e0cb55ac19270
Ariel Betti <ariel.betti@gmail.com>
2023-02-06 23:52:40 -0300

    chore: components individual imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
 import Button from "./Button";
 import Card from "./Card";
+import PopMenu from "./PopMenu";
+import Header from "./Header";
 import LoadingStatus from "./LoadingStatus";
 import ProfilePicture from "./ProfilePicture";
 import Sidebar from "./Sidebar";
@@ -8,6 +10,8 @@ import UserStatus from "./UserStatus";
 export {
   Button,
   Card,
+  PopMenu,
+  Header,
   LoadingStatus,
   ProfilePicture,
   Sidebar,
