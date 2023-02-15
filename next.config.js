0c7264fd861425e2edbce1f3464a9d8b7138e5a4
Ariel Betti <ariel.betti@gmail.com>
2023-02-15 00:55:30 -0300

    chore: core project configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: { appDir: true },
+  transpilePackages: ['react-draft-wysiwyg', 'draft-js'],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
