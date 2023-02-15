e43d5c6e3f8390e456ce076d56372ab368cc5ba7
Ariel Betti <ariel.betti@gmail.com>
2023-02-15 01:02:28 -0300

    feat: add new application interfaces and types

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,60 +1,91 @@
 export interface IUser {
-  name: string,
-  email: string,
-  picture?: string,
+  sessionId: string;
+  name: string;
+  email: string;
+  picture?: string;
 }
 
+export type TUserStatus = 'OFFLINE' | 'ONLINE';
+
+export type TMessagesChanel = {
+  schema: string;
+  table: string;
+  commit_timestamp: string;
+  eventType: string;
+  new: TMessage;
+  old: TMessage;
+  errors: any;
+};
+
+export type TMessage = {
+  id: number;
+  inserted_at: string;
+  message: string;
+  user_id: string;
+  channel_id: number;
+  users: {
+    username: string,
+    picture?: string,
+  }
+};
+
+export type TChannel = {
+  id: number;
+  title: string;
+  description?: string;
+};
+
 export interface TGoogleUserEndpoint {
-  id: string
-  aud: string
-  role: string
-  email: string
-  email_confirmed_at: string
-  phone: string
-  confirmed_at: string
-  last_sign_in_at: string
-  app_metadata: TGoogleUserAppMetadata
-  user_metadata: TGoogleUserMetadata
-  identities: TGoogleUserIdentity[]
-  created_at: string
-  updated_at: string
+  id: string;
+  aud: string;
+  role: string;
+  email: string;
+  email_confirmed_at: string;
+  phone: string;
+  confirmed_at: string;
+  last_sign_in_at: string;
+  app_metadata: TGoogleUserAppMetadata;
+  user_metadata: TGoogleUserMetadata;
+  identities: TGoogleUserIdentity[];
+  created_at: string;
+  updated_at: string;
 }
 
 export interface TGoogleUserAppMetadata {
-  provider: string
-  providers: string[]
+  provider: string;
+  providers: string[];
 }
 
 export interface TGoogleUserMetadata {
-  avatar_url: string
-  email: string
-  email_verified: boolean
-  full_name: string
-  iss: string
-  name: string
-  picture: string
-  provider_id: string
-  sub: string
+  avatar_url: string;
+  email: string;
+  email_verified: boolean;
+  full_name: string;
+  iss: string;
+  name: string;
+  picture: string;
+  provider_id: string;
+  sub: string;
 }
 
 export interface TGoogleUserIdentity {
-  id: string
-  user_id: string
-  identity_data: TGoogleUserIdentityData
-  provider: string
-  last_sign_in_at: string
-  created_at: string
-  updated_at: string
+  id: string;
+  user_id: string;
+  identity_data: TGoogleUserIdentityData;
+  provider: string;
+  last_sign_in_at: string;
+  created_at: string;
+  updated_at: string;
 }
 
 export interface TGoogleUserIdentityData {
-  avatar_url: string
-  email: string
-  email_verified: boolean
-  full_name: string
-  iss: string
-  name: string
-  picture: string
-  provider_id: string
-  sub: string
+  avatar_url: string;
+  email: string;
+  email_verified: boolean;
+  full_name: string;
+  iss: string;
+  name: string;
+  picture: string;
+  provider_id: string;
+  sub: string;
 }
