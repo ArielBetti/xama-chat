d8e7a6ae1a2451e00662e8bfe625470d479c4823
Ariel Betti <ariel.betti@gmail.com>
2023-02-15 01:02:02 -0300

    feat(side-bar): add new user status type to component props

--- src/components/Sidebar/types.ts
+++ src/components/Sidebar/types.ts
@@ -1,7 +1,8 @@
-import { IUser } from "@/interfaces"
+import { IUser, TUserStatus } from "@/interfaces"
 import { ReactNode } from "react"
 
 export type TSidebar = {
   header: ReactNode,
   user: IUser | null,
+  userStatus: TUserStatus,
 }
